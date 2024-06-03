b3db356a6a9f18954b5fb1843cf6891660f38504
mulinatti <gabrielmulinari2002@gmail.com>
2024-06-03 18:14:36 -0300

    add: checkbox input motorista

--- src/components/CadastroAjudante.tsx
+++ src/components/CadastroAjudante.tsx
@@ -16,6 +16,7 @@ import { ptBR } from "date-fns/locale";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { format } from "date-fns";
 import { CalendarIcon } from "lucide-react";
+import { Checkbox } from "./ui/checkbox";
 
 const formSchema = z.object({
   nome: z.string().min(1, {
@@ -24,7 +25,7 @@ const formSchema = z.object({
   apelido: z.string().max(20, {
     message: "Apelido muito grande",
   }),
-  motorista: z.boolean(),
+  motorista: z.boolean().default(false).optional(),
   dataNascimento: z.date(),
   telefone: z.string().min(1, {
     message: "Telefone não pode estar vazio",
@@ -32,7 +33,6 @@ const formSchema = z.object({
 });
 
 const CadastroAjudante = () => {
-  //adicionar checkbox de motorista
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -46,7 +46,11 @@ const CadastroAjudante = () => {
   });
 
   const cadastrarAjudante = (ajudante: z.infer<typeof formSchema>) => {
-    console.log(ajudante.dataNascimento.toLocaleDateString("pt-BR"));
+    const ajudanteFixed = {
+      ...ajudante,
+      dataNascimento: ajudante.dataNascimento.toLocaleDateString("pt-BR"),
+    };
+    console.log(ajudanteFixed);
   };
 
   return (
@@ -95,51 +99,67 @@ const CadastroAjudante = () => {
               </FormItem>
             )}
           />
-          <FormField
-          control={form.control}
-          name="dataNascimento"
-          render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <FormLabel>Data de nascimento</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
+          <fieldset className="flex items-center justify-between">
+            <FormField
+              control={form.control}
+              name="dataNascimento"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Data de nascimento</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={"outline"}
+                          className={`w-[240px] pl-3 text-left font-normal
+                          ${!field.value && "text-muted-foreground"}`}
+                        >
+                          {field.value ? (
+                            format(field.value, "dd/MM/yyyy")
+                          ) : (
+                            <span>Escolha uma data</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        locale={ptBR}
+                        captionLayout="dropdown-buttons"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        fromYear={1950}
+                        toYear={new Date().getFullYear()}
+                        disabled={(date) =>
+                          date > new Date() || date < new Date("1900-01-01")
+                        }
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="motorista"
+              render={({ field }) => (
+                <FormItem className="flex items-center gap-3 mt-4">
+                  <FormLabel className="mt-2">Motorista</FormLabel>
                   <FormControl>
-                    <Button
-                      variant={"outline"}
-                      className={
-                        `w-[240px] pl-3 text-left font-normal
-                        ${!field.value && "text-muted-foreground"}`
-                      }
-                    >
-                      {field.value ? (
-                        format(field.value, "dd/MM/yyyy")
-                      ) : ( 
-                        <span>Escolha uma data</span>
-                      )}
-                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
                   </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                  <Calendar
-                    mode="single"
-                    locale={ptBR}
-                    captionLayout="dropdown-buttons"
-                    selected={field.value}
-                    onSelect={field.onChange}
-                    fromYear={1960}
-                    toYear={2030}
-                    disabled={(date) =>
-                      date > new Date() || date < new Date("1900-01-01")
-                    } 
-                    initialFocus
-                  />
-                </PopoverContent>
-              </Popover>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </fieldset>
           <Button type="submit">Cadastrar</Button>
         </form>
       </Form>
